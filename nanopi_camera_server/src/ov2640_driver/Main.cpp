ad3f48714e65f9c172d7b0b4b9cf8eea22dec7ce
Przemek <pwalczykk@gmail.com>
2016-08-29 12:29:43 +0200

    first test - compilatioin

---
+++ nanopi_camera_server/src/ov2640_driver/Main.cpp
@@ -0,0 +1,21 @@
+#include "ov2640_driver.hpp"
+#include "ImagePublisher.hpp"
+
+#include <ros/ros.h>
+
+int main(int argc, char** argv){
+
+    ros::init(argc, argv, "ov2640_driver");
+    ros::NodeHandle nh;
+    ros::Rate loop_rate(10);
+
+    ImagePublisher cam1("/nanopi/cam1", &nh);
+
+    cam1.Init();
+
+    while(ros::ok()){
+        cam1.PublishImage();
+        loop_rate.sleep();
+    }
+
+}
